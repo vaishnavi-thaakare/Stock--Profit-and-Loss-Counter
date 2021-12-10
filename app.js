a084fbeaaeaf39ccc05f6dfee8f4814e65b0eae0
vaishnavithakare2807 <vaishnavithaakare@gmail.com>
2021-12-10 10:51:45 +0530

    update

--- app.js
+++ app.js
@@ -3,42 +3,25 @@ var quantity = document.querySelector("#quantity");
 var currentPrice = document.querySelector("#Current-price");
 var browseBtn = document.querySelector("#btn");
 var outputDiv = document.querySelector("#output");
-// console.log(initialPrice);
-// console.log(quantity);
-// console.log(outputDiv);
-// console.log(browsebtn);
-// console.log(currentPrice)
-
-
-
-
-
 
 
 browseBtn.addEventListener("click", calculateProfitAddLoss)
 
 function calculateProfitAddLoss() {
     
-    // profit logic
-    if (Number(initialPrice.value) >Number(currentPrice.value)){
+    
+    if (Number(initialPrice.value) <Number(currentPrice.value)){
         var profit=(initialPrice.value-currentPrice.value)*quantity.value;
-        var profitPercentage=(profit/initialPrice.value)*quantity.value;
+        var profitPercentage=(profit/initialPrice.value)*100;
         outputDiv.innerText=(`The profit is ${profit} and profit percentage is ${profitPercentage}%`)
         
-    }else if (Number(initialPrice.value) <Number(currentPrice.value)){
+    }else if (Number(initialPrice.value) >Number(currentPrice.value)){
         var loss=(currentPrice.value-initialPrice.value)*quantity.value;
-        var lossPercentage=(loss/initialPrice.value)*quantity.value;
+        var lossPercentage=(loss/initialPrice.value)*100;
         outputDiv.innerText=(`The loss is ${loss} and loss percentage is ${lossPercentage}%`)
         
     }else{
         outputDiv.innerText=("No pain no gain ")
     }
 
-
-    // loss logic
-
-
-
-
-    
 }
